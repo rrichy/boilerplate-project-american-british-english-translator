9be219e0a0fc236215f8e795bc7858cbc009f3c2
rrichy <rasulrichy@gmail.com>
2021-05-01 13:54:35 +0800

    update

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,43 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+  translate(text, locale) {
+    if(text === '') return { error: 'No text to translate' };
+    if(!text || !locale) return { error: 'Required field(s) missing' };
+    if(!['american-to-british', 'british-to-american'].includes(locale)) return { error: 'Invalid value for locale field' };
 
+    let bucket = [];
+    if(locale === 'american-to-british') {
+      Object.keys(americanOnly).forEach(usEng => {
+        if(text.includes(usEng)) bucket.push([usEng, americanOnly[usEng]]);
+      });
+      let words = text.split(' ');
+      for(let i = 0; i < words.length; i++) {
+        let time = words[i].match(/\d{1,2}[.:]\d{1,2}/g);
+        if(time) time.forEach(a => bucket.push([time, this.parseTime(a, locale)]));
+        else {
+          if(americanToBritishSpelling.hasOwnProperty(words[i])) bucket.push([words[i], americanToBritishSpelling[words[i]]]);
+          if(americanToBritishTitles.hasOwnProperty(words[i])) bucket.push([words[i], americanToBritishTitles[words[i]]]);
+        }
+      }
+    }
+    else {
+      Object.keys(britishOnly).forEach(ukEng => {
+        if(text.includes(ukEng)) bucket.push(ukEng);
+      });
+    }
+
+    console.log(bucket);
+
+   
+
+    /* If text requires no translation, return "Everything looks good to me!" for the translation value. */
+  }
+
+  parseTime(time, locale) {
+    if(locale === 'american-to-british') return time.slice(0, 2) + '.' + time.slice(3);
+    return time.slice(0, 2) + ':' + time.slice(3);
+  }
 }
 
 module.exports = Translator;
